e0305c5986532fc41b72600625d30222a5f4152e
Vik63 <sfoto116@yandex.ru>
2026-04-26 19:37:25 +0400

    добавил русский шрифт

---
+++ src/ESP32S3_OM_V6.ino
@@ -0,0 +1,27 @@
+// #include "zTimesNRItalic36.h"
+// #include "zCalibri36.h"
+
+// #define AA_FONT_TIME zTimesNRItalic36
+// #define AA_FONT_CALI zCalibri36
+
+// #include <TFT_eSPI.h>
+
+// TFT_eSPI tft = TFT_eSPI();
+
+// void setup() {
+//   tft.begin();
+//   tft.setRotation(1);
+
+//   tft.fillScreen(TFT_BLACK);
+//   tft.setTextColor(TFT_WHITE, TFT_BLACK);  // Set the font colour AND the background colour
+//   tft.setCursor(5, 5);                     // Set cursor at top left of screen
+
+//   tft.loadFont(AA_FONT_TIME);     // Must load the font first
+//   tft.println("Широкая электрификация южных губерний даст мощный толчок подъёму сельского хозяйства.");
+
+//   tft.loadFont(AA_FONT_CALI);     // Must load the font first
+//   tft.println("Широкая электрификация южных губерний даст мощный толчок подъёму сельского хозяйства.");
+// }
+
+// void loop() {
+// }
